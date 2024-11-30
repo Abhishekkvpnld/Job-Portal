892e01fb96f4b6bb5924c44ddbd1deb33d8000c0
Abhishekkv <abhishekkvpnld@gmail.com>
2024-11-30 15:29:00 +0530

    db connected

--- server/app.js
+++ server/app.js
@@ -2,13 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import dbConnection from "./utils/dbConnection.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
@@ -17,6 +25,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+dbConnection().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
 });
--- server/utils/dbConnection.js
+++ server/utils/dbConnection.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI_LOCAL, {});
+    console.log("Server connected to database successfully...📅");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const dbConnection = async()=>{
-    try {
-        await mongoose.connect("",{})
-    } catch (error) {
-        console.log(error);
-    }
-}
+export default dbConnection;
