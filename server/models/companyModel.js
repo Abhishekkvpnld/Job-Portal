9a3f7db2aa94959965898009273910165238fd99
Abhishekkv <abhishekkvpnld@gmail.com>
2024-11-30 16:12:48 +0530

    all model created

---
+++ server/models/companyModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Company = mongoose.model("Company", companySchema);
+export default Company;
