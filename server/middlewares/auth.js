c291ce1f745d5dcc08f13604d49abfde09751a5c
Abhishekkv <abhishekkvpnld@gmail.com>
2025-03-26 14:52:35 +0530

    jwt error fixed

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const jwtAuth = (req, res, next) => {
   try {
     // Retrieve token from cookies
-    const token = req.cookies?.token;
+    const token = req.cookies?.token || req.headers?.cookie;
     if (!token) {
       return res.status(401).json({
         success: false,
